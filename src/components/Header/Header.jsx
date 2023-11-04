c65d270fe6485fffd53d2d4b30c2204fea49add1
bmnazmussakib <nazmussakib161@gmail.com>
2023-11-04 12:47:15 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 
 export default function Header({ handleDelete, selectedItem, setConfirm }) {
-
   return (
     <>
-      <nav class="navbar navbar-light bg-light">
+      <nav class="navbar navbar-light bg-light sticky-top" style={{height: '80px'}}>
         <div class="container">
           <div className="d-flex align-items-center">
-            {selectedItem && selectedItem.length > 0 && (
-              <input type="checkbox" checked />
+            {selectedItem && selectedItem.length > 0 ? (
+              <>
+                <input type="checkbox" checked />{" "}
+                <h5 className="ms-3 mb-0">
+                  {selectedItem.length} Files Selected
+                </h5>
+              </>
+            ) : (
+              <h5 className="ms-3 mb-0">Image Gallery</h5>
             )}
-            <h5 className="ms-3 mb-0">{selectedItem.length} Files Selected</h5>
           </div>
-          <button className="btn btn-danger" disabled={selectedItem.length === 0} data-mdb-toggle="modal" data-mdb-target="#exampleModal">
-            Delete
-          </button>
+          {selectedItem.length > 0 && (
+            <button
+              className="btn btn-danger"
+              data-mdb-toggle="modal"
+              data-mdb-target="#exampleModal"
+            >
+              Delete
+            </button>
+          )}
         </div>
       </nav>
-
-     
     </>
   );
 }
